1719da947f084469089f184b2e58399b7db822c8
Paco <kifouetobosmwansilapacochristi@gmail.com>
2025-08-22 23:50:41 +0100

    project

---
+++ script/pacoenglish.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.querySelector('.menu-toggle').addEventListener('click', function() {
+        document.querySelector('#navbar').classList.toggle('active');
+    });
+};
